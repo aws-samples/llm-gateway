a237b2f0f138956dd6c071ed1a1bfaeb12a8a904
Andrew J. Young <andrewjunyoung@gmail.com>
2024-10-29 15:41:11 -0400

    Added deployment script.

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create 2 certificates and register them with Route53.
+cd cdk
+cdk deploy --app "certs"
+cd ..
+
+# Run the main deployment script to create cloud infrastructure
+cd cdk
+. ./deploy.sh
+cd ..
+
+# Create a Cognito admin user
+cd scripts
+python scripts/create_cognito_user.py -u "admin" -p "Password123!"
+cd ..
